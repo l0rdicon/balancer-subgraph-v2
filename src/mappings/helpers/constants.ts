d057f4f54f365dad8f34d46e292358985799f5b4
beethovenxfi <beethovenxfi@gmail.com>
2021-09-20 12:13:40 +0200

    change deployment to the custom graph-node

--- src/mappings/helpers/constants.ts
+++ src/mappings/helpers/constants.ts
@@ -29,7 +29,7 @@ export class AddressByNetwork {
 let network: string = dataSource.network();
 
 let vaultAddressByNetwork: AddressByNetwork = {
-  mainnet: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+  mainnet: '0x20dd72Ed959b6147912C2e529F0a0C651c33c9ce',
   kovan: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
   goerli: '0x65748E8287Ce4B9E6D83EE853431958851550311',
   rinkeby: '0xF07513C68C55A31337E3b58034b176A15Dce16eD',
@@ -40,7 +40,7 @@ let vaultAddressByNetwork: AddressByNetwork = {
 };
 
 let wethAddressByNetwork: AddressByNetwork = {
-  mainnet: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  mainnet: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
   kovan: '0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1',
   goerli: '0x9A1000D492d40bfccbc03f413A48F5B6516Ec0Fd',
   rinkeby: '0x80dD2B80FbcFB06505A301d732322e987380EcD6',
@@ -51,7 +51,7 @@ let wethAddressByNetwork: AddressByNetwork = {
 };
 
 let wbtcAddressByNetwork: AddressByNetwork = {
-  mainnet: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+  mainnet: '0x321162Cd933E2Be498Cd2267a90534A804051b11',
   kovan: '0x1C8E3Bcb3378a443CC591f154c5CE0EBb4dA9648',
   goerli: '0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86',
   rinkeby: '0xb4761d0481B4f7a8A858D2796eEF3DAa2f3D9D2c',
@@ -62,7 +62,7 @@ let wbtcAddressByNetwork: AddressByNetwork = {
 };
 
 let usdAddressByNetwork: AddressByNetwork = {
-  mainnet: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
+  mainnet: '0x04068da6c83afcfa0e13ba15a6696662335d5b75', // USDC
   kovan: '0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115',
   goerli: '0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86',
   rinkeby: '0x70b55Af71B29c5Ca7e67bD1995250364C4bE5554',
@@ -73,7 +73,7 @@ let usdAddressByNetwork: AddressByNetwork = {
 };
 
 let usdcAddressByNetwork: AddressByNetwork = {
-  mainnet: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  mainnet: '0x04068da6c83afcfa0e13ba15a6696662335d5b75',
   kovan: '0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115',
   goerli: '0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86',
   rinkeby: '0x70b55Af71B29c5Ca7e67bD1995250364C4bE5554',
@@ -84,18 +84,18 @@ let usdcAddressByNetwork: AddressByNetwork = {
 };
 
 let balAddressByNetwork: AddressByNetwork = {
-  mainnet: '0xba100000625a3754423978a60c9317c58a424e3D',
+  mainnet: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
   kovan: '0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7',
   goerli: '0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86',
   rinkeby: '0x8850Fd0C65d9B2B168153FAc6bAa269A566c4ef7',
   polygon: '0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3',
   fuji: '0x9c2eE4065F5BcaF2220c6eA788829eEd80aec503',
   dev: '0xf702269193081364E355f862f2CFbFCdC5DB738C',
-  fantom: '', //TODO: BEETS token?
+  fantom: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83', //TODO: BEETS token?
 };
 
 let daiAddressByNetwork: AddressByNetwork = {
-  mainnet: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  mainnet: '0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e',
   kovan: '0x04DF6e4121c27713ED22341E7c7Df330F56f289B',
   goerli: '0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86',
   rinkeby: '0x12c615406F20eDcBDa50888f9fd6734dC4836417',
